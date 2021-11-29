84f99af3d784f0570483848a629b38ab201cb874
mfabianb <mfabianb08@gmail.com>
2021-11-28 23:20:38 -0600

    Se agregaron headers al controlador

--- src/main/java/mx/ipn/escom/pizarron/crud/controller/CrudController.java
+++ src/main/java/mx/ipn/escom/pizarron/crud/controller/CrudController.java
@@ -2,8 +2,10 @@ package mx.ipn.escom.pizarron.crud.controller;
 
 import mx.ipn.escom.pizarron.crud.domain.api.CrudServicePort;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +17,14 @@ public class CrudController {
     private CrudServicePort crudServicePort;
 
     @GetMapping("/info")
-    public String activityRecordDetail() {
-        return crudServicePort.saludar();
+    public ResponseEntity<String> activityRecordDetail() {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.set("Access-Control-Allow-Origin", "*");
+        responseHeaders.set("Access-Control-Allow-Methods", "GET,POST,OPTIONS,DELETE,PUT");
+        responseHeaders.set("Access-Control-Allow-Headers", "X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+        responseHeaders.set("Content-Type", "application/json");
+        String sss = crudServicePort.saludar();
+        return new ResponseEntity<String>(sss, responseHeaders, HttpStatus.ACCEPTED);
     }
 
 }
